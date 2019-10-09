ceda81ec416ed4fb3e39e64ec86f5a3e3a879ec8
Dima Smirnov <smirnov@adobe.com>
2019-10-09 12:40:21 -0700

    Typo

--- dps_3d/datasets.py
+++ dps_3d/datasets.py
@@ -42,7 +42,7 @@ class ShapenetDataset(th.utils.data.Dataset):
             distance_fields = distance_fields[5:-5,5:-5,5:-5]
             alignment_fields = alignment_fields[5:-5,5:-5,5:-5]
 
-        occupancy_fields = utils.compute_occupancy_fields(occupancy_fields)
+        occupancy_fields = utils.compute_occupancy_fields(distance_fields)
 
         return {
             'fname': fname,
